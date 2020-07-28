b757d4d9f5399af233f61026975f09492035c15d
Ephraim Kigamba <ekigamba@ona.io>
2020-07-28 09:59:41 +0300

    Fix errors in TaskRegisterAdapter

--- opensrp-tasking/src/main/java/org/smartregister/tasking/adapter/TaskRegisterAdapter.java
+++ opensrp-tasking/src/main/java/org/smartregister/tasking/adapter/TaskRegisterAdapter.java
@@ -10,17 +10,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.apache.commons.lang3.StringUtils;
 import org.smartregister.domain.Task;
-import org.smartregister.reveal.R;
-import org.smartregister.reveal.model.CardDetails;
-import org.smartregister.reveal.model.TaskDetails;
-import org.smartregister.reveal.util.CardDetailsUtil;
-import org.smartregister.reveal.util.Constants.Intervention;
-import org.smartregister.reveal.viewholder.TaskRegisterViewHolder;
+import org.smartregister.tasking.R;
+import org.smartregister.tasking.model.CardDetails;
+import org.smartregister.tasking.model.TaskDetails;
+import org.smartregister.tasking.util.CardDetailsUtil;
+import org.smartregister.tasking.util.Constants.Intervention;
+import org.smartregister.tasking.viewholder.TaskRegisterViewHolder;
+import org.smartregister.tasking.model.TaskDetails;
+import org.smartregister.tasking.viewholder.TaskRegisterViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.smartregister.reveal.util.Constants.BusinessStatus.NOT_ELIGIBLE;
+import static org.smartregister.tasking.util.Constants.BusinessStatus.NOT_ELIGIBLE;
 
 /**
  * Created by samuelgithengi on 3/20/19.
@@ -57,7 +59,7 @@ public class TaskRegisterAdapter extends RecyclerView.Adapter<TaskRegisterViewHo
                 name = task.getFamilyName() != null ? task.getFamilyName() : task.getStructureName() != null ? task.getStructureName() : context.getString(R.string.unenumerated_structure);
             }
             action = context.getString(R.string.record_status);
-        } else if (Intervention.MOSQUITO_COLLECTION.equals(task.getTaskCode())) {
+        } /*else if (Intervention.MOSQUITO_COLLECTION.equals(task.getTaskCode())) {
             name = context.getString(R.string.mosquito_collection_point);
             action = context.getString(R.string.record_mosquito_collection);
         } else if (Intervention.LARVAL_DIPPING.equals(task.getTaskCode())) {
@@ -79,7 +81,7 @@ public class TaskRegisterAdapter extends RecyclerView.Adapter<TaskRegisterViewHo
             if (task.getBusinessStatus() != null) {
                 action = CardDetailsUtil.getTranslatedBusinessStatus(task.getBusinessStatus()).replaceAll(" ", "\n");
             }
-        } else {
+        }*/ else {
             name = NOT_ELIGIBLE.equals(task.getBusinessStatus()) ? context.getString(R.string.ineligible_location) : task.getFamilyName();
             if (name == null) {
                 name = task.getStructureName() != null ? task.getStructureName() : context.getString(R.string.unenumerated_structure);
