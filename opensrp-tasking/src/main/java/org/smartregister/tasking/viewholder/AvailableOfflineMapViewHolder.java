0d214c1897d7961d04e143ba58fb4f70181e7a69
Ephraim Kigamba <ekigamba@ona.io>
2020-07-30 17:51:39 +0300

    Fix references errors in AvailableOfflineMapsFragment

---
+++ opensrp-tasking/src/main/java/org/smartregister/tasking/viewholder/AvailableOfflineMapViewHolder.java
@@ -0,0 +1,13 @@
+package org.smartregister.tasking.viewholder;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public class AvailableOfflineMapViewHolder extends BaseOfflineMapViewHolder {
+
+    public AvailableOfflineMapViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+}
