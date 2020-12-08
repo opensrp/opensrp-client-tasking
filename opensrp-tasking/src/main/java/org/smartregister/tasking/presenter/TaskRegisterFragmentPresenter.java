da3baa428391b0a02b5ad1c9bd57c6e96b941a98
Ephraim Kigamba <ekigamba@ona.io>
2020-12-08 17:36:10 +0300

    Remove dummy task data
    - Fixes https://github.com/OpenSRP/opensrp-client-goldsmith/issues/29

--- opensrp-tasking/src/main/java/org/smartregister/tasking/presenter/TaskRegisterFragmentPresenter.java
+++ opensrp-tasking/src/main/java/org/smartregister/tasking/presenter/TaskRegisterFragmentPresenter.java
@@ -201,7 +201,7 @@ public class TaskRegisterFragmentPresenter extends BaseFormFragmentPresenter imp
             this.tasks = tasks;
 
             // TODO: Remove this static data
-            if (tasks == null || tasks.isEmpty()) {
+            /*if (tasks == null || tasks.isEmpty()) {
                 TaskDetails taskDetails = new TaskDetails("3459-sdfa23-sdqasdf");
 
                 //Map<String, String> map = ImmutableMap.of("first_name", "Martin", "last_name", "Bull", "birthdate", "1970-01-10", "phone_number", "07246738839", "gender", "Male");
@@ -213,7 +213,7 @@ public class TaskRegisterFragmentPresenter extends BaseFormFragmentPresenter imp
                 tasks = new ArrayList<>();
                 tasks.add(taskDetails);
                 this.tasks = tasks;
-            }
+            }*/
 
             if (tasks == null) {
 
